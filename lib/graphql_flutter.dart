12df775b6680d1d46e03ef4e9caf6db678ff2d4b
Zino Hofmann <zino@hofmann.amsterdam>
2018-06-21 17:55:27 +0200

    ✨ Add provider widget to save and restore the in-memory cache

--- lib/graphql_flutter.dart
+++ lib/graphql_flutter.dart
@@ -4,5 +4,6 @@ export './src/client.dart';
 
 export './src/cache/in_memory.dart';
 
-export './src/components/query.dart';
-export './src/components/mutation.dart';
+export './src/widgets/query.dart';
+export './src/widgets/mutation.dart';
+export './src/widgets/cache_provider.dart';
