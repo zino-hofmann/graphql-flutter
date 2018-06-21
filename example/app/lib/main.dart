8cfefaa391014797ea075afd88ba687d32bebd15
Zino Hofmann <zino@hofmann.amsterdam>
2018-06-21 18:05:16 +0200

    📝 Update the example to refelct changes in the code.

--- example/app/lib/main.dart
+++ example/app/lib/main.dart
@@ -14,12 +14,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+    return new CacheProvider(
+      child: new MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
