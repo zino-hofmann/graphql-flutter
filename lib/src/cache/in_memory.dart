32998abacaab6bc84842a2dd086bafd0d4f3ac00
Zino Hofmann <zino@hofmann.amsterdam>
2018-06-21 17:33:21 +0200

    ✨ Add options to write memory to file

--- lib/src/cache/in_memory.dart
+++ lib/src/cache/in_memory.dart
@@ -1,7 +1,62 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
 class InMemoryCache {
   Map<String, dynamic> _inMemoryCache = new Map<String, dynamic>();
 
+  Future<String> get _localStoragePath async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localStorageFile async {
+    final String path = await _localStoragePath;
+
+    return File('$path/cache.txt');
+  }
+
+  Future<File> _writeToStorage() async {
+    final File file = await _localStorageFile;
+    final String output = json.encode(_inMemoryCache);
+
+    print('STORAGE_OUT:');
+    print(output);
+
+    return file.writeAsString(output);
+  }
+
+  Future<Map<String, dynamic>> _readFromStorage() async {
+    try {
+      final File file = await _localStorageFile;
+      final String content = await file.readAsString();
+
+      print('STORAGE_IN:');
+      print(content);
+
+      return json.decode(content);
+    } catch (error) {
+      // TODO: handle error
+      print(error);
+
+      return new Map<String, dynamic>();
+    }
+  }
+
   bool hasEntity(String key) => _inMemoryCache.containsKey(key);
+
+  void save() async {
+    print("CACHE: SAVE");
+
+    await _writeToStorage();
+  }
+
+  void restore() async {
+    _inMemoryCache = await _readFromStorage();
+  }
 
   dynamic read(String key) {
     print("CACHE: READ");
