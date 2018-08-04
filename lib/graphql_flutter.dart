7ed012eb62c3d69ab0f2a8f23da6ce66f97e6dd3
Aleksandar Faraj <AleksandarFaraj@users.noreply.github.com>
2018-08-04 03:31:47 +0200

    GraphqlConsumer missing from library
    Am I the only one using this library?

--- lib/graphql_flutter.dart
+++ lib/graphql_flutter.dart
@@ -10,6 +10,7 @@ export 'package:graphql_flutter/src/websocket/socket.dart';
 
 export 'package:graphql_flutter/src/widgets/cache_provider.dart';
 export 'package:graphql_flutter/src/widgets/graphql_provider.dart';
+export 'package:graphql_flutter/src/widgets/graphql_consumer.dart';
 export 'package:graphql_flutter/src/widgets/query.dart';
 export 'package:graphql_flutter/src/widgets/mutation.dart';
 export 'package:graphql_flutter/src/widgets/subscription.dart';
