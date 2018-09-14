0d9e6e060d989a25e4e02bd74e7957173df8cbc8
Zino Hofmann <zino@hofmann.amsterdam>
2018-09-14 13:27:16 +0200

    📖 Improved the inline docs of the client

--- lib/src/graphql_client.dart
+++ lib/src/graphql_client.dart
@@ -10,14 +10,18 @@ import 'package:graphql_flutter/src/core/query_options.dart';
 import 'package:graphql_flutter/src/link/link.dart';
 import 'package:graphql_flutter/src/cache/cache.dart';
 
-/// The link is a [Link] over which GraphQL documents will be resolved into a response.
+/// The link is a [Link] over which GraphQL documents will be resolved into a [FetchResult].
 /// The cache is the initial [Cache] to use in the data store.
 class GraphQLClient {
+  /// The [Link] over which GraphQL documents will be resolved into a [FetchResult].
   final Link link;
+
+  /// The initial [Cache] to use in the data store.
   final Cache cache;
 
   QueryManager queryManager;
 
+  /// Constructs a [GraphQLClient] given a [Link] and a [Cache].
   GraphQLClient({
     @required this.link,
     @required this.cache,
@@ -28,23 +32,25 @@ class GraphQLClient {
     );
   }
 
+  /// This registers a query in the [QueryManager] and returns an [ObservableQuery]
+  /// based on the provided [WatchQueryOptions].
   ObservableQuery watchQuery(WatchQueryOptions options) {
     return queryManager.watchQuery(options);
   }
 
-  /// This resolves a single query according to the options specified and
-  /// returns a [Future] which resolves with the resulting data or throws an error.
+  /// This resolves a single query according to the [QueryOptions] specified and
+  /// returns a [Future] which resolves with the [QueryResult] or throws an [Exception].
   Future<QueryResult> query(QueryOptions options) {
     return queryManager.query(options);
   }
 
-  /// This resolves a single mutation according to the options specified and returns a
-  /// [Future] which resolves with the resulting data or throws an error.
+  /// This resolves a single mutation according to the [MutationOptions] specified and
+  /// returns a [Future] which resolves with the [QueryResult] or throws an [Exception].
   Future<QueryResult> mutate(MutationOptions options) {
     return queryManager.mutate(options);
   }
 
-  /// This subscribes to a graphql subscription according to the options specified and returns an
+  /// This subscribes to a GraphQL subscription according to the options specified and returns an
   /// [Stream] which either emits received data or an error.
   Stream<dynamic> subscribe(dynamic options) {
     // TODO: merge the subscription client with the new client
