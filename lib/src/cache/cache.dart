095dcfb46c364a80890b14d962ea261eaf121274
Rex Momoh <juicycleff@gmail.com>
2019-03-04 16:59:22 +0100

    added an abstract method to the cache

--- lib/src/cache/cache.dart
+++ lib/src/cache/cache.dart
@@ -1,14 +1,14 @@
 abstract class Cache {
   dynamic read(String key) {}
 
+  Future<bool> remove(String key, bool cascade) async {}
+  
   void write(
     String key,
     dynamic value,
   ) {}
 
   void save() {}
-
   void restore() {}
-
   void reset() {}
 }
