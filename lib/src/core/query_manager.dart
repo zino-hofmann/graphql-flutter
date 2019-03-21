3b162b64f0938c71300056848437a90259c12bea
M. Walker Wells <github@mwalkerwells.com>
2019-03-21 11:15:50 -0700

    Verify errors are not null before JSON transform

--- lib/src/core/query_manager.dart
+++ lib/src/core/query_manager.dart
@@ -192,9 +192,11 @@ class QueryManager {
     List<GraphQLError> errors;
 
     if (fetchResult.errors != null) {
-      errors = List<GraphQLError>.from(fetchResult.errors.map<GraphQLError>(
-        (dynamic rawError) => GraphQLError.fromJSON(rawError),
-      ));
+      errors = List<GraphQLError>.from(
+        fetchResult.errors
+          .where((error) => error != null)
+          .map<GraphQLError>((dynamic rawError) => GraphQLError.fromJSON(rawError))
+      );
     }
 
     return QueryResult(
