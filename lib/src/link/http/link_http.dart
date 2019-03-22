7490139d787dccb8a7ac69706a10c9397922bb6a
M. Walker Wells <git@mwalkerwells.com>
2019-03-21 18:38:51 -0700

    Removing the type annotation

--- lib/src/link/http/link_http.dart
+++ lib/src/link/http/link_http.dart
@@ -206,8 +206,7 @@ FetchResult _parseResponse(Response response) {
   final FetchResult fetchResult = FetchResult();
 
   if (jsonResponse['errors'] != null) {
-    fetchResult.errors =
-      (jsonResponse['errors'] as List<Map<String, dynamic>>)
+    fetchResult.errors = jsonResponse['errors']
       .where((error) => error != null);
   }
 
