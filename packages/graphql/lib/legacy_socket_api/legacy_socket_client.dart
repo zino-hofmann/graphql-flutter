7a81ede5f00c91f4b75f2bd7b3531959c5f8386c
micimize <rosenthalm93@gmail.com>
2019-06-05 10:57:09 -0500

    move legacy websocket code into  submodule

--- packages/graphql/lib/legacy_socket_api/legacy_socket_client.dart
+++ packages/graphql/lib/legacy_socket_api/legacy_socket_client.dart
@@ -1,21 +1,29 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
-import 'package:meta/meta.dart';
-import 'package:websocket/websocket.dart' show WebSocket, WebSocketStatus;
 
+import 'package:meta/meta.dart';
 import 'package:rxdart/rxdart.dart';
+
 import 'package:uuid_enhanced/uuid.dart';
 
 import 'package:graphql/src/websocket/messages.dart';
 
+enum SocketConnectionState { NOT_CONNECTED, CONNECTING, CONNECTED }
+
+@deprecated
 class SocketClientConfig {
   const SocketClientConfig({
     this.autoReconnect = true,
     this.queryAndMutationTimeout = const Duration(seconds: 10),
     this.inactivityTimeout = const Duration(seconds: 30),
     this.delayBetweenReconnectionAttempts = const Duration(seconds: 5),
+    this.compression = CompressionOptions.compressionDefault,
     this.initPayload,
+    // @todo please review this ignore rule
+    // ignore: deprecated_member_use_from_same_package
+    @deprecated this.legacyInitPayload,
   });
 
   /// Whether to reconnect to the server after detecting connection loss.
@@ -38,15 +46,35 @@ class SocketClientConfig {
   // If null, no timeout is applied, although not recommended.
   final Duration queryAndMutationTimeout;
 
+  final CompressionOptions compression;
+
   /// The initial payload that will be sent to the server upon connection.
   /// Can be null, but must be a valid json structure if provided.
   final dynamic initPayload;
 
-  InitOperation get initOperation => InitOperation(initPayload);
+  final Map<String, dynamic> legacyInitPayload;
+
+  InitOperation get initOperation {
+    if (legacyInitPayload != null) {
+      print(
+        'WARNING: Using a legacyInitPayload which will be removed soon. '
+        'If you need this particular payload serialization behavior, '
+        'please comment on this issue with details on your usecase: '
+        'https://github.com/zino-app/graphql-flutter/pull/277',
+      );
+      // @todo please review this ignore rule
+      // ignore: deprecated_member_use_from_same_package
+      return LegacyInitOperation(legacyInitPayload);
+    }
+    return InitOperation(initPayload);
+  }
 }
 
-enum SocketConnectionState { NOT_CONNECTED, CONNECTING, CONNECTED }
-
+/// @deprecated Old SocketClient that accepts and handles headers.
+/// WebSocket headers are not usable in the browser.
+/// So, to encourage universality,
+/// they are not usable in the main socket link and client any longer
+///
 /// Wraps a standard web socket instance to marshal and un-marshal the server /
 /// client payloads into dart object representation.
 ///
@@ -55,12 +83,16 @@ enum SocketConnectionState { NOT_CONNECTED, CONNECTING, CONNECTED }
 /// It is meant to be instantiated once, and you can let this class handle all the heavy-
 /// lifting of socket state management. Once you're done with the socket connection, make sure
 /// you call the [dispose] method to release all allocated resources.
+@deprecated
 class SocketClient {
   SocketClient(
     this.url, {
     this.protocols = const <String>[
       'graphql-ws',
     ],
+    this.headers = const <String, String>{
+      'content-type': 'application/json',
+    },
     this.config = const SocketClientConfig(),
     @visibleForTesting this.randomBytesForUuid,
   }) {
@@ -71,13 +103,22 @@ class SocketClient {
   final String url;
   final SocketClientConfig config;
   final Iterable<String> protocols;
+  final Map<String, dynamic> headers;
   final BehaviorSubject<SocketConnectionState> _connectionStateController =
       BehaviorSubject<SocketConnectionState>();
 
   Timer _reconnectTimer;
   WebSocket _socket;
+
   @visibleForTesting
   WebSocket get socket => _socket;
+
+  Stream<dynamic> _stream;
+
+  @visibleForTesting
+  @protected
+  Stream<dynamic> get stream => _stream ??= _socket.asBroadcastStream();
+
   Stream<GraphQLSocketMessage> _messageStream;
 
   StreamSubscription<ConnectionKeepAlive> _keepAliveSubscription;
@@ -95,16 +136,16 @@ class SocketClient {
     print('Connecting to websocket: $url...');
 
     try {
-      _socket = await WebSocket.connect(
-        url,
-        protocols: protocols,
-      );
+      _socket = await WebSocket.connect(url,
+          protocols: protocols,
+          headers: headers,
+          compression: config.compression);
       _connectionStateController.value = SocketConnectionState.CONNECTED;
       print('Connected to websocket.');
       _write(config.initOperation);
 
-      _messageStream =
-          _socket.stream.map<GraphQLSocketMessage>(_parseSocketMessage);
+      _stream ??= _socket.asBroadcastStream();
+      _messageStream = _stream.map<GraphQLSocketMessage>(_parseSocketMessage);
 
       if (config.inactivityTimeout != null) {
         _keepAliveSubscription = _messagesOfType<ConnectionKeepAlive>().timeout(
@@ -133,14 +174,11 @@ class SocketClient {
             print('error: $e');
           });
     } catch (e) {
-      onConnectionLost(e);
+      onConnectionLost();
     }
   }
 
-  void onConnectionLost([e]) {
-    if (e != null) {
-      print('There was an error causing connection lost: $e');
-    }
+  void onConnectionLost() {
     print('Disconnected from websocket.');
     _reconnectTimer?.cancel();
     _keepAliveSubscription?.cancel();
@@ -181,12 +219,11 @@ class SocketClient {
   Future<void> dispose() async {
     print('Disposing socket client..');
     _reconnectTimer?.cancel();
-    await Future.wait([
-      _socket?.close(),
-      _keepAliveSubscription?.cancel(),
-      _messageSubscription?.cancel(),
-      _connectionStateController?.close(),
-    ]);
+    await _socket?.close();
+    await _keepAliveSubscription?.cancel();
+    await _messageSubscription?.cancel();
+    await _connectionStateController?.close();
+    _stream = null;
   }
 
   static GraphQLSocketMessage _parseSocketMessage(dynamic message) {
@@ -348,4 +385,24 @@ class SocketClient {
   Stream<M> _messagesOfType<M extends GraphQLSocketMessage>() => _messageStream
       .where((GraphQLSocketMessage message) => message is M)
       .cast<M>();
+}
+
+/// The old implementation of [InitOperation]
+// @todo please review this ignore rule
+// ignore: deprecated_member_use_from_same_package
+@deprecated
+class LegacyInitOperation extends InitOperation {
+  LegacyInitOperation(dynamic payload) : super(payload);
+
+  @override
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> jsonMap = <String, dynamic>{};
+    jsonMap['type'] = type;
+
+    if (payload != null) {
+      jsonMap['payload'] = json.encode(payload);
+    }
+
+    return jsonMap;
+  }
 }
