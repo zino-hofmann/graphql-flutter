cf81ec3698e5c96c25b2bc45c4ed359bd5637481
micimize <rosenthalm93@gmail.com>
2019-10-01 09:48:18 -0500

    refactor: add message on failed document assertion

--- packages/graphql/lib/src/core/raw_operation_data.dart
+++ packages/graphql/lib/src/core/raw_operation_data.dart
@@ -13,7 +13,10 @@ class RawOperationData {
     @required this.document,
     Map<String, dynamic> variables,
     String operationName,
-  })  : assert(document != null),
+  })  : assert(
+          document != null,
+          'document option is required. You must specify your GraphQL document in the query options.',
+        ),
         _operationName = operationName,
         variables = SplayTreeMap<String, dynamic>.of(
           variables ?? const <String, dynamic>{},
