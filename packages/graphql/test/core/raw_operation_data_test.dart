53de348049545289f54851da9617b22a983310b2
Klāvs Priedītis <klavs@prieditis.lv>
2019-10-06 14:25:33 +0300

    test(client): add tests for `operationName` getter
    Signed-off-by: Klāvs Priedītis <klavs@prieditis.lv>

---
+++ packages/graphql/test/core/raw_operation_data_test.dart
@@ -0,0 +1,125 @@
+import 'package:graphql/src/core/raw_operation_data.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('operation name', () {
+    group('single operation', () {
+      test('query without name', () {
+        final opData = RawOperationData(
+          document: 'query {}',
+        );
+
+        expect(opData.operationName, null);
+      });
+
+      test('query with explicit name', () {
+        final opData = RawOperationData(
+          document: 'query Operation {}',
+          operationName: 'Operation',
+        );
+
+        expect(opData.operationName, 'Operation');
+      });
+
+      test('mutation with explicit name', () {
+        final opData = RawOperationData(
+          document: 'mutation Operation {}',
+          operationName: 'Operation',
+        );
+
+        expect(opData.operationName, 'Operation');
+      });
+
+      test('subscription with explicit name', () {
+        final opData = RawOperationData(
+          document: 'subscription Operation {}',
+          operationName: 'Operation',
+        );
+
+        expect(opData.operationName, 'Operation');
+      });
+
+      test('query with implicit name', () {
+        final opData = RawOperationData(
+          document: 'query Operation {}',
+        );
+
+        expect(opData.operationName, 'Operation');
+      });
+
+      test('mutation with implicit name', () {
+        final opData = RawOperationData(
+          document: 'mutation Operation {}',
+        );
+
+        expect(opData.operationName, 'Operation');
+      });
+
+      test('subscription with implicit name', () {
+        final opData = RawOperationData(
+          document: 'subscription Operation {}',
+        );
+
+        expect(opData.operationName, 'Operation');
+      });
+    });
+    group('multiple operations', () {
+      const document = r'''
+        query OperationQ {}
+        mutation OperationM {}
+        subscription OperationS {}
+      ''';
+
+      test('query with explicit name', () {
+        final opData = RawOperationData(
+          document: document,
+          operationName: 'OperationQ',
+        );
+
+        expect(opData.operationName, 'OperationQ');
+      });
+
+      test('mutation with explicit name', () {
+        final opData = RawOperationData(
+          document: document,
+          operationName: 'OperationM',
+        );
+
+        expect(opData.operationName, 'OperationM');
+      });
+
+      test('subscription with explicit name', () {
+        final opData = RawOperationData(
+          document: document,
+          operationName: 'OperationS',
+        );
+
+        expect(opData.operationName, 'OperationS');
+      });
+
+      test('query with implicit name', () {
+        final opData = RawOperationData(
+          document: document,
+        );
+
+        expect(opData.operationName, 'OperationS');
+      });
+
+      test('mutation with implicit name', () {
+        final opData = RawOperationData(
+          document: document,
+        );
+
+        expect(opData.operationName, 'OperationS');
+      });
+
+      test('subscription with implicit name', () {
+        final opData = RawOperationData(
+          document: document,
+        );
+
+        expect(opData.operationName, 'OperationS');
+      });
+    });
+  });
+}
