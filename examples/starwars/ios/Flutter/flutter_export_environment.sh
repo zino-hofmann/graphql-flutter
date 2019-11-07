25f3c85ef904a1467ca0a46733b3eec085dbd143
Maina Wycliffe <wmmaina7@gmail.com>
2019-11-07 22:13:50 +0300

    docs: ignore generated files

--- examples/starwars/ios/Flutter/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\tools\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\mw\projects\opensource\graphql-flutter\examples\starwars"
-export "FLUTTER_TARGET=lib\main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\tools\flutter\bin\cache\artifacts\engine\ios"
