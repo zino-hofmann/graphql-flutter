7d538e16dd626d2ff7c4f963031ff6c825f24269
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2020-01-31 14:22:23 +0100

    fix(client): Throw a ClientException on non-json responses, to be  consistent with other malformed responses.  Fix tests to also expect a ClientException in theses cases.  Fixes #552.

--- packages/graphql/lib/src/link/http/link_http.dart
+++ packages/graphql/lib/src/link/http/link_http.dart
@@ -318,8 +318,12 @@ Future<FetchResult> _parseResponse(StreamedResponse response) async {
   final Uint8List responseByte = await response.stream.toBytes();
   final String decodedBody = encoding.decode(responseByte);
 
-  final Map<String, dynamic> jsonResponse =
-      json.decode(decodedBody) as Map<String, dynamic>;
+  Map<String, dynamic> jsonResponse;
+  try {
+      jsonResponse=  json.decode(decodedBody) as Map<String, dynamic>;
+  }catch(e){
+    throw ClientException('Invalid response body: $decodedBody');
+  }
   final FetchResult fetchResult = FetchResult();
 
   if (jsonResponse['errors'] != null) {
