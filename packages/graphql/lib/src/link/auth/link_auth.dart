167fac5366160aa8384c3d87c900b38b065f6d59
micimize <rosenthalm93@gmail.com>
2020-05-09 17:09:41 -0500

    feat(graphql): custom auth headerKey

--- packages/graphql/lib/src/link/auth/link_auth.dart
+++ packages/graphql/lib/src/link/auth/link_auth.dart
@@ -3,13 +3,13 @@ import 'dart:async';
 import 'package:graphql/src/link/link.dart';
 import 'package:graphql/src/link/operation.dart';
 import 'package:graphql/src/link/fetch_result.dart';
+import 'package:meta/meta.dart';
 
 typedef GetToken = FutureOr<String> Function();
 
 class AuthLink extends Link {
-  AuthLink({
-    this.getToken,
-  }) : super(
+  AuthLink({@required this.getToken, this.headerKey = 'Authorization'})
+      : super(
           request: (Operation operation, [NextLink forward]) {
             StreamController<FetchResult> controller;
 
@@ -18,7 +18,7 @@ class AuthLink extends Link {
                 final String token = await getToken();
 
                 operation.setContext(<String, Map<String, String>>{
-                  'headers': <String, String>{'Authorization': token}
+                  'headers': <String, String>{headerKey: token}
                 });
               } catch (error) {
                 controller.addError(error);
@@ -35,4 +35,5 @@ class AuthLink extends Link {
         );
 
   GetToken getToken;
+  String headerKey;
 }
