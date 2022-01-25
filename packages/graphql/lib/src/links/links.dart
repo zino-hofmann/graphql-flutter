9af573759a0d94dfaf8c92f9d821004c795cd640
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-01-25 22:48:34 +0100

    graphql: reworking Socket Client API to support custom header
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- packages/graphql/lib/src/links/links.dart
+++ packages/graphql/lib/src/links/links.dart
@@ -1,6 +1,4 @@
 // Reexport all gql_links
 export 'package:graphql/src/links/gql_links.dart';
-
 export 'package:graphql/src/links/auth_link.dart';
-
 export 'package:graphql/src/links/websocket_link/websocket_link.dart';
--- packages/graphql/lib/src/utilities/platform_html.dart
+++ packages/graphql/lib/src/utilities/platform_html.dart
@@ -2,9 +2,11 @@ import 'package:graphql/src/links/websocket_link/websocket_client.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 Future<WebSocketChannel> defaultConnectPlatform(
-  Uri uri,
-  Iterable<String>? protocols,
-) async {
+    Uri uri, Iterable<String>? protocols,
+    {Map<String, dynamic>? headers}) async {
+  if (headers != null) {
+    print("The headers on the web are not supported");
+  }
   final webSocketChannel =
       await WebSocketChannel.connect(uri, protocols: protocols);
   return webSocketChannel.forGraphQL();
