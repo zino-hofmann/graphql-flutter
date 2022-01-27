727dc35ed2aeeef976b57b4404f96ef380e54ed5
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-01-27 13:12:27 +0100

    packages: increase pub dev dependeces
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- packages/graphql_flutter/lib/graphql_flutter.dart
+++ packages/graphql_flutter/lib/graphql_flutter.dart
@@ -1,6 +1,7 @@
 library graphql_flutter;
 
 export 'package:graphql/client.dart';
+export 'package:graphql_flutter/src/hive_init.dart';
 
 export 'package:graphql_flutter/src/widgets/cache_provider.dart';
 export 'package:graphql_flutter/src/widgets/graphql_consumer.dart';
@@ -9,5 +10,3 @@ export 'package:graphql_flutter/src/widgets/mutation.dart';
 export 'package:graphql_flutter/src/widgets/query.dart';
 export 'package:graphql_flutter/src/widgets/subscription.dart';
 export 'package:graphql_flutter/src/widgets/result_accumulator.dart';
-
-export 'package:graphql_flutter/src/hive_init.dart';
