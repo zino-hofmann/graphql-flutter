b15a7d640a2c5e9fdaba47eecbacdf845fdea4d0
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-02-14 22:17:29 +0100

    graphql_flutter: adding missing import
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- packages/graphql_flutter/lib/src/widgets/hooks/mutation.dart
+++ packages/graphql_flutter/lib/src/widgets/hooks/mutation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:graphql/client.dart';
 import 'package:graphql_flutter/src/widgets/hooks/graphql_client.dart';
+import 'package:graphql_flutter/src/widgets/hooks/watch_query.dart';
 
 typedef RunMutation<TParsed> = MultiSourceResult<TParsed> Function(
   Map<String, dynamic> variables, {
