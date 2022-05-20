c0effc7691a2b1aa922f0c081f983e6c4abfeddb
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-05-20 11:49:54 +0200

    prepare version 5.1.1-beta.3
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- examples/starwars/lib/generated_plugin_registrant.dart
+++ examples/starwars/lib/generated_plugin_registrant.dart
@@ -4,6 +4,7 @@
 
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
 
 import 'package:connectivity_plus_web/connectivity_plus_web.dart';
 
