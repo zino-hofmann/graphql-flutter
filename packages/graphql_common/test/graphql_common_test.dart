4ecfa64fce56b9b4e232aba33d0feacb3d2a7a65
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-08-22 01:06:01 +0100

    feat(graphql_common): add common package that contains utils functions
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ packages/graphql_common/test/graphql_common_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {});
+  });
+}
